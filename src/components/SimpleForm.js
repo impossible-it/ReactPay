6ecf26e3e2f1dd74a002883abc3fd28f7f9ef0e2
impossible-it <talezfaq@gmail.com>
2024-11-24 16:50:29 +0200

    '24.11simple'

--- src/components/SimpleForm.js
+++ src/components/SimpleForm.js
@@ -48,14 +48,14 @@ const SimpleForm = () => {
   };
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-fon px-4">
-      <div className="container mx-auto p-4">
-        <h2 className="text-2xl font-bold mb-12 text-center">Выберите опцию и укажите сумму</h2>
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-8 justify-items-center">
+    <div className="flex items-center justify-center min-h-screen bg-gray-fon px-2">
+      <div className="container mx-auto p-2">
+        <h2 className="text-2xl font-bold mb-4 text-center">Выберите опцию и укажите сумму</h2>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-4 justify-items-center">
           <div
-            className={`bg-white p-8 rounded-lg transition transform cursor-pointer ${formData.selectedCard === 'card1' ? 'shadow-md bg-grayth hover:shadow-2xl' : 'hover:shadow-lg'} hover:scale-105`}
+            className={`bg-white p-4 rounded-lg transition transform cursor-pointer ${formData.selectedCard === 'card1' ? 'shadow-md bg-grayth hover:shadow-2xl' : 'hover:shadow-lg'} hover:scale-105`}
             onClick={() => handleCardSelect('card1')}
-            style={{ width: '95%', maxWidth: '70%', flexDirection: 'column', display: 'flex', alignItems: 'center' }}
+            style={{ width: '95%', maxWidth: 'none', flexDirection: 'column', display: 'flex', alignItems: 'center' }}
           >
             <img src={FastsystemImage} alt="Card 1" className="object-contain h-48 w-48 mb-4" />
             <button className={`py-2 px-4 rounded-md text-white font-bold ${formData.selectedCard === 'card1' ? 'text-purpleth' : 'text-purpleth hover:bg-purple-950'}`}>
@@ -63,9 +63,9 @@ const SimpleForm = () => {
             </button>
           </div>
           <div
-            className={`bg-white p-8 rounded-lg transition transform cursor-pointer ${formData.selectedCard === 'card2' ? 'shadow-md bg-grayth hover:shadow-2xl' : 'hover:shadow-lg'} hover:scale-105`}
+            className={`bg-white p-4 rounded-lg transition transform cursor-pointer ${formData.selectedCard === 'card2' ? 'shadow-md bg-grayth hover:shadow-2xl' : 'hover:shadow-lg'} hover:scale-105`}
             onClick={() => handleCardSelect('card2')}
-            style={{ width: '95%', maxWidth: '70%', flexDirection: 'column', display: 'flex', alignItems: 'center' }}
+            style={{ width: '95%', maxWidth: 'none', flexDirection: 'column', display: 'flex', alignItems: 'center' }}
           >
             <img src={CardImage} alt="Card 2" className="object-contain h-48 w-48 mb-4" />
             <button className={`py-2 px-4 rounded-md text-white font-bold ${formData.selectedCard === 'card2' ? 'text-purpleth' : 'text-purpleth hover:bg-purple-950'}`}>
@@ -73,9 +73,9 @@ const SimpleForm = () => {
             </button>
           </div>
           <div
-            className={`bg-white p-8 rounded-lg transition transform cursor-pointer ${formData.selectedCard === 'card3' ? 'shadow-md bg-grayth hover:shadow-2xl' : 'hover:shadow-lg'} hover:scale-105`}
+            className={`bg-white p-4 rounded-lg transition transform cursor-pointer ${formData.selectedCard === 'card3' ? 'shadow-md bg-grayth hover:shadow-2xl' : 'hover:shadow-lg'} hover:scale-105`}
             onClick={() => handleCardSelect('card3')}
-            style={{ width: '95%', maxWidth: '70%', flexDirection: 'column', display: 'flex', alignItems: 'center' }}
+            style={{ width: '95%', maxWidth: 'none', flexDirection: 'column', display: 'flex', alignItems: 'center' }}
           >
             <img src={TerminalImage} alt="Card 3" className="object-contain h-48 w-48 mb-4" />
             <button className={`py-2 px-4 rounded-md text-white font-bold ${formData.selectedCard === 'card3' ? 'text-purpleth' : 'text-purpleth hover:bg-purple-950'}`}>
@@ -83,9 +83,9 @@ const SimpleForm = () => {
             </button>
           </div>
         </div>
-        <div className="flex justify-center bg-white m-16">
+        <div className="flex justify-center bg-white m-8">
           <form onSubmit={handleSubmit} className="w-full max-w-md p-6 rounded-lg">
-            <div className="mb-8">
+            <div className="mb-4">
               <label htmlFor="amount" className="block text-sm font-normal text-gray-700">
                 Сумма сделки в рублях
               </label>
