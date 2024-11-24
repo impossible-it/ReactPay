ff323634c624279fc4f3488b9e018e73115440c6
impossible-it <talezfaq@gmail.com>
2024-11-24 18:02:27 +0200

    '24.11simple'

--- src/components/SimpleForm.js
+++ src/components/SimpleForm.js
@@ -11,7 +11,6 @@ const SimpleForm = () => {
     amount: ''
   });
   const navigate = useNavigate();
-  const presetAmounts = [6200, 12000, 21500];
 
   useEffect(() => {
     localStorage.setItem('return', 1); // Storing in localStorage
@@ -48,37 +47,34 @@ const SimpleForm = () => {
     }
   };
 
-  const setPresetAmount = (amount) => {
-    setFormData({
-      ...formData,
-      amount
-    });
-  };
-
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-fon px-2">
-      <div className="container mx-auto p-2">
-        <h2 className="text-2xl font-bold mb-4 text-center mt-4 md:mt-0">Выберите опцию и укажите сумму</h2>
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-4 justify-items-center md:justify-center">
+      <div className="container mx-auto p-4">
+        <h2 className="text-2xl font-bold mb-12 text-center">Выберите опцию и укажите сумму</h2>
+        <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 gap-8 justify-items-center">
           {[{ img: FastsystemImage, text: 'По номеру телефона', card: 'card1' },
             { img: CardImage, text: 'Карта банка', card: 'card2' },
             { img: TerminalImage, text: 'Эквайринг', card: 'card3' }].map((item, index) => (
             <div
               key={index}
-              className={`bg-white rounded-lg transition transform cursor-pointer flex flex-col md:flex-row ${formData.selectedCard === item.card ? 'shadow-md bg-grayth hover:shadow-2xl' : 'hover:shadow-lg'} hover:scale-105`}
+              className={`bg-white p-8 rounded-lg ${formData.selectedCard === item.card ? 'shadow-md bg-grayth' : ''} md:flex md:flex-col border-0 md:w-[100%] w-[70%] cursor-pointer transition transform ${formData.selectedCard === item.card ? 'hover:shadow-2xl' : 'hover:shadow-lg'} hover:scale-105`}
               onClick={() => handleCardSelect(item.card)}
-              style={{ width: '100%', maxWidth: 'none', flexDirection: 'row', display: 'flex', alignItems: 'center' }}
+              style={{ flexDirection: 'column', alignItems: 'center', display: 'flex' }}
             >
-              <button className={`py-2 px-4 rounded-md text-white font-bold flex-grow ${formData.selectedCard === item.card ? 'text-purpleth' : 'text-purpleth hover:bg-purple-950'}`} style={{ width: '60%' }}>
+              <div className="flex justify-center items-center md:h-60 h-20 w-25 md:w-60 mb-0 md:mb-10">
+                <img src={item.img} alt={`Card ${index + 1}`} className="h-25 md:h-41 w-25 md:w-56 object-contain" />
+              </div>
+              <button
+                className={`py-2 px-4 mb-0 md:mb-10 rounded-md w-36 h-14 text-white font-bold ${formData.selectedCard === item.card ? 'text-purpleth' : 'text-purpleth hover:bg-purple-950 hover:text-white'}`}
+              >
                 {item.text}
               </button>
-              <img src={item.img} alt={`Card ${index + 1}`} className="object-contain h-32 w-auto md:w-48 md:mb-4 flex-shrink-0" style={{ width: '40%' }} />
             </div>
           ))}
         </div>
-        <div className="flex justify-center bg-white m-0 md:m-8">
+        <div className="flex justify-center bg-white m-16">
           <form onSubmit={handleSubmit} className="w-full max-w-md p-6 rounded-lg">
-            <div className="mb-4">
+            <div className="mb-8">
               <label htmlFor="amount" className="block text-sm font-normal text-gray-700">
                 Сумма сделки в рублях
               </label>
@@ -88,22 +84,14 @@ const SimpleForm = () => {
                 id="amount"
                 value={formData.amount}
                 onChange={handleChange}
-                className="mt-1 block w-full border bg-gray-form border-gray-200 rounded-md shadow-sm p-2 focus:bg-white focus:border-neutral-700 focus:outline-none"
+                className="mt-1 block w-full max-w-md border bg-gray-form border-gray-200 rounded-md shadow-sm p-2 hover:shadow-md focus:bg-white focus:border-neutral-700 focus:outline-none"
                 required
               />
-              <div className="flex justify-around mt-4">
-                {presetAmounts.map(amount => (
-                  <button type="button" key={amount} onClick={() => setPresetAmount(amount)}
-                    className="text-gray-500 border rounded p-2 hover:bg-gray-200">
-                    {amount} руб.
-                  </button>
-                ))}
-              </div>
             </div>
             <button
               type="submit"
-              className="bg-grayth h-[50px] mt-4 font-bold text-white py-2 px-4 rounded-lg hover:bg-purple-950 w-full"
-              disabled={!formData.amount}
+              className="bg-grayth h-[50px] mt-4 font-bold text-white py-2 px-4 rounded-lg hover:bg-purple-950 w-full mb-12"
+              disabled={!formData.selectedCard}
             >
               Далее
             </button>
