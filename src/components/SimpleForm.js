4e6ed9b794344451728248950fd357b5090cc430
impossible-it <talezfaq@gmail.com>
2024-11-24 16:46:40 +0200

    '24.11simple'

--- src/components/SimpleForm.js
+++ src/components/SimpleForm.js
@@ -7,13 +7,13 @@ import './styles.css';
 
 const SimpleForm = () => {
   const [formData, setFormData] = useState({
-    selectedCard: 'card2', // Выбор по умолчанию
+    selectedCard: 'card2', // Default selection
     amount: ''
   });
   const navigate = useNavigate();
 
   useEffect(() => {
-    localStorage.setItem('return', 1); // Добавление в localStorage
+    localStorage.setItem('return', 1); // Storing in localStorage
   }, []);
 
   const handleChange = (e) => {
@@ -50,48 +50,35 @@ const SimpleForm = () => {
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-fon px-4">
       <div className="container mx-auto p-4">
-        <h2 className="text-2xl font-bold mb-12 text-center">Оберіть опцию та вкажіть скільки грошей мяу</h2>
-        <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 gap-8 justify-items-center">
+        <h2 className="text-2xl font-bold mb-12 text-center">Выберите опцию и укажите сумму</h2>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-8 justify-items-center">
           <div
-            className={`bg-white p-8 rounded-lg ${formData.selectedCard === 'card1' ? 'shadow-md bg-grayth' : ''} md:flex md:flex-col border-0 md:w-[100%] w-[70%] cursor-pointer transition transform ${formData.selectedCard === 'card1' ? 'hover:shadow-2xl' : 'hover:shadow-lg'} hover:scale-105`}
+            className={`bg-white p-8 rounded-lg transition transform cursor-pointer ${formData.selectedCard === 'card1' ? 'shadow-md bg-grayth hover:shadow-2xl' : 'hover:shadow-lg'} hover:scale-105`}
             onClick={() => handleCardSelect('card1')}
-            style={{ display: 'flex', flexDirection: 'row-reverse', alignItems: 'center', }}
+            style={{ width: '95%', maxWidth: '70%', flexDirection: 'column', display: 'flex', alignItems: 'center' }}
           >
-            <div className="flex justify-center items-center h-20 md:h-60 md:w-60 w-25 md:w-60 ">
-              <img src={FastsystemImage} alt="Card 1" className="h-20 md:h-48 w-25 md:w-48 object-contain" />
-            </div>
-            
-            <button
-              className={`py-2 px-4 rounded-md w-36 h-14 text-white font-bold ${formData.selectedCard === 'card1' ? 'text-purpleth' : 'text-purpleth hover:bg-purple-950 hover:text-white'}`}
-            >
+            <img src={FastsystemImage} alt="Card 1" className="object-contain h-48 w-48 mb-4" />
+            <button className={`py-2 px-4 rounded-md text-white font-bold ${formData.selectedCard === 'card1' ? 'text-purpleth' : 'text-purpleth hover:bg-purple-950'}`}>
               По номеру телефона
             </button>
           </div>
           <div
-            className={`bg-white p-8 rounded-lg ${formData.selectedCard === 'card2' ? 'shadow-md bg-grayth' : ''} border-0 md:w-[100%] w-[70%] cursor-pointer transition transform ${formData.selectedCard === 'card2' ? 'hover:shadow-2xl' : 'hover:shadow-lg'} hover:scale-105`}
+            className={`bg-white p-8 rounded-lg transition transform cursor-pointer ${formData.selectedCard === 'card2' ? 'shadow-md bg-grayth hover:shadow-2xl' : 'hover:shadow-lg'} hover:scale-105`}
             onClick={() => handleCardSelect('card2')}
-            style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}
+            style={{ width: '95%', maxWidth: '70%', flexDirection: 'column', display: 'flex', alignItems: 'center' }}
           >
-            <div className="flex justify-center items-center md:h-60 h-20 w-25 md:w-60 mb-0 md:mb-10">
-              <img src={CardImage} alt="Card 2" className="h-25 md:h-41 w-25 md:w-56 object-contain" />
-            </div>
-            <button
-              className={`py-2 px-4 mb-0 md:mb-10 rounded-md w-36 h-14 text-white font-bold ${formData.selectedCard === 'card2' ? 'text-purpleth' : 'text-purpleth hover:bg-purple-950 hover:text-white'}`}
-            >
+            <img src={CardImage} alt="Card 2" className="object-contain h-48 w-48 mb-4" />
+            <button className={`py-2 px-4 rounded-md text-white font-bold ${formData.selectedCard === 'card2' ? 'text-purpleth' : 'text-purpleth hover:bg-purple-950'}`}>
               Карта банка
             </button>
           </div>
           <div
-            className={`bg-white p-8 rounded-lg ${formData.selectedCard === 'card3' ? 'shadow-md bg-grayth' : ''} border-0 md:w-[100%] w-[70%] cursor-pointer transition transform ${formData.selectedCard === 'card3' ? 'hover:shadow-2xl' : 'hover:shadow-lg'} hover:scale-105`}
+            className={`bg-white p-8 rounded-lg transition transform cursor-pointer ${formData.selectedCard === 'card3' ? 'shadow-md bg-grayth hover:shadow-2xl' : 'hover:shadow-lg'} hover:scale-105`}
             onClick={() => handleCardSelect('card3')}
-            style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}
+            style={{ width: '95%', maxWidth: '70%', flexDirection: 'column', display: 'flex', alignItems: 'center' }}
           >
-            <div className="flex justify-center items-center h-60 w-60 mb-0 md:mb-10">
-              <img src={TerminalImage} alt="Card 3" className="h-50 w-50 object-contain" />
-            </div>
-            <button
-              className={`py-2 px-4 mb-0 md:mb-10 rounded-md w-36 h-14 text-white font-bold ${formData.selectedCard === 'card3' ? 'text-purpleth' : 'text-purpleth hover:bg-purple-950 hover:text-white'}`}
-            >
+            <img src={TerminalImage} alt="Card 3" className="object-contain h-48 w-48 mb-4" />
+            <button className={`py-2 px-4 rounded-md text-white font-bold ${formData.selectedCard === 'card3' ? 'text-purpleth' : 'text-purpleth hover:bg-purple-950'}`}>
               Эквайринг
             </button>
           </div>
@@ -108,14 +95,14 @@ const SimpleForm = () => {
                 id="amount"
                 value={formData.amount}
                 onChange={handleChange}
-                className="mt-1 block w-full max-w-md border bg-gray-form border-gray-200 rounded-md shadow-sm p-2 hover:shadow-md focus:bg-white focus:border-neutral-700 focus:outline-none"
+                className="mt-1 block w-full border bg-gray-form border-gray-200 rounded-md shadow-sm p-2 focus:bg-white focus:border-neutral-700 focus:outline-none"
                 required
               />
             </div>
             <button
               type="submit"
-              className="bg-grayth h-[50px] mt-4 font-bold text-white py-2 px-4 rounded-lg hover:bg-purple-950 w-full mb-12"
-              disabled={!formData.selectedCard}
+              className="bg-grayth h-[50px] mt-4 font-bold text-white py-2 px-4 rounded-lg hover:bg-purple-950 w-full"
+              disabled={!formData.amount}
             >
               Далее
             </button>
