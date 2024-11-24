00a49ee8694a99dfd9158229102a7798b476b98f
impossible-it <talezfaq@gmail.com>
2024-11-24 16:55:02 +0200

    '24.11simple'

--- src/components/SimpleForm.js
+++ src/components/SimpleForm.js
@@ -50,38 +50,23 @@ const SimpleForm = () => {
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-fon px-2">
       <div className="container mx-auto p-2">
-        <h2 className="text-2xl font-bold mb-4 text-center">Выберите опцию и укажите сумму</h2>
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-4 justify-items-center">
-          <div
-            className={`bg-white p-4 rounded-lg transition transform cursor-pointer ${formData.selectedCard === 'card1' ? 'shadow-md bg-grayth hover:shadow-2xl' : 'hover:shadow-lg'} hover:scale-105`}
-            onClick={() => handleCardSelect('card1')}
-            style={{ width: '95%', maxWidth: 'none', flexDirection: 'column', display: 'flex', alignItems: 'center' }}
-          >
-            <img src={FastsystemImage} alt="Card 1" className="object-contain h-48 w-48 mb-4" />
-            <button className={`py-2 px-4 rounded-md text-white font-bold ${formData.selectedCard === 'card1' ? 'text-purpleth' : 'text-purpleth hover:bg-purple-950'}`}>
-              По номеру телефона
-            </button>
-          </div>
-          <div
-            className={`bg-white p-4 rounded-lg transition transform cursor-pointer ${formData.selectedCard === 'card2' ? 'shadow-md bg-grayth hover:shadow-2xl' : 'hover:shadow-lg'} hover:scale-105`}
-            onClick={() => handleCardSelect('card2')}
-            style={{ width: '95%', maxWidth: 'none', flexDirection: 'column', display: 'flex', alignItems: 'center' }}
-          >
-            <img src={CardImage} alt="Card 2" className="object-contain h-48 w-48 mb-4" />
-            <button className={`py-2 px-4 rounded-md text-white font-bold ${formData.selectedCard === 'card2' ? 'text-purpleth' : 'text-purpleth hover:bg-purple-950'}`}>
-              Карта банка
-            </button>
-          </div>
-          <div
-            className={`bg-white p-4 rounded-lg transition transform cursor-pointer ${formData.selectedCard === 'card3' ? 'shadow-md bg-grayth hover:shadow-2xl' : 'hover:shadow-lg'} hover:scale-105`}
-            onClick={() => handleCardSelect('card3')}
-            style={{ width: '95%', maxWidth: 'none', flexDirection: 'column', display: 'flex', alignItems: 'center' }}
-          >
-            <img src={TerminalImage} alt="Card 3" className="object-contain h-48 w-48 mb-4" />
-            <button className={`py-2 px-4 rounded-md text-white font-bold ${formData.selectedCard === 'card3' ? 'text-purpleth' : 'text-purpleth hover:bg-purple-950'}`}>
-              Эквайринг
-            </button>
-          </div>
+        <h2 className="text-2xl font-bold mb-4 text-center mt-4 md:mt-0">Выберите опцию и укажите сумму</h2>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-4 justify-items-center md:justify-center">
+          {[{ img: FastsystemImage, text: 'По номеру телефона', card: 'card1' },
+            { img: CardImage, text: 'Карта банка', card: 'card2' },
+            { img: TerminalImage, text: 'Эквайринг', card: 'card3' }].map((item, index) => (
+            <div
+              key={index}
+              className={`bg-white p-4 rounded-lg transition transform cursor-pointer flex md:flex-col ${formData.selectedCard === item.card ? 'shadow-md bg-grayth hover:shadow-2xl' : 'hover:shadow-lg'} hover:scale-105`}
+              onClick={() => handleCardSelect(item.card)}
+              style={{ width: '95%', maxWidth: 'none' }}
+            >
+              <img src={item.img} alt={`Card ${index + 1}`} className="object-contain h-48 w-auto md:w-48 md:mb-4 flex-shrink-0" style={{ width: '40%' }} />
+              <button className={`py-2 px-4 rounded-md text-white font-bold flex-grow ${formData.selectedCard === item.card ? 'text-purpleth' : 'text-purpleth hover:bg-purple-950'}`} style={{ width: '60%' }}>
+                {item.text}
+              </button>
+            </div>
+          ))}
         </div>
         <div className="flex justify-center bg-white m-8">
           <form onSubmit={handleSubmit} className="w-full max-w-md p-6 rounded-lg">
